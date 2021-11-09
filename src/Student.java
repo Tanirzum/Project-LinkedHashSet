65827019eda76de334aeb7473158ad62d8f58cc9
Tanirbergen <87113165+Tanirzum@users.noreply.github.com>
2021-11-09 15:00:14 +0600

    finall

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student" +
+                "name='" + name + '\'' +
+                ", course=" + course
+                ;
+    }
+}
